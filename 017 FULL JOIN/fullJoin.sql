0f322ca45b25f287d3ebd98283f86c7fb0bbf245
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-21 14:41:32 -0500

    Update

--- 017 FULL JOIN/fullJoin.sql
+++ 017 FULL JOIN/fullJoin.sql
@@ -1,1 +1,6 @@
-dflkgjdfgklj
+-- FULL JOIN: selecciond e todo
+Select * from esquema."PERSONAS"
+
+SELECT "NOMBRE", "APELLIDO1", "DEPARTAMENTO"
+from esquema."PERSONAS" full join "esquema"."DEPARTAMENTOS"
+on esquema."PERSONAS"."DEP" = "esquema"."DEPARTAMENTOS"."DEP"
