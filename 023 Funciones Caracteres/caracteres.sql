67455b945f7d975031a74f2c56d3b12080ce915e
BrianMarquez3 <brian3marquez@gmail.com>
2021-05-21 15:45:15 -0500

    Funciones caracteres+

--- 023 Funciones Caracteres/caracteres.sql
+++ 023 Funciones Caracteres/caracteres.sql
@@ -1,1 +1,15 @@
-fgdfgdfg
+-- CARACTERES
+
+SELECT * FROM esquema."PERSONAS"
+
+/* conteo de caracteres*/
+select "NOMBRE" ,length ("NOMBRE") AS "longitud"
+from esquema."PERSONAS"
+
+/* minusculas*/
+select "NOMBRE" ,LOWER ("NOMBRE") AS "minusculas"
+from esquema."PERSONAS"
+
+select "NOMBRE" ,left ("NOMBRE",3) AS "longitud"
+from esquema."PERSONAS"
+
